b43787f5314cd7210ab969eeebc3581020b79c8d
punderplus <ilya.obukhov.m@gmail.com>
2022-04-30 02:00:32 +0300

    Added LWC map/some fixes

---
+++ force-app/main/default/lwc/locationMap/locationMap.js
@@ -0,0 +1,23 @@
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+
+const LOCATION_LATITUDE_FIELD = 'Voting_Location__c.Latitude__c';
+const LOCATION_LONGITUDE_FIELD = 'Voting_Location__c.Longitude__c';
+
+const locationFields = [
+	LOCATION_LATITUDE_FIELD,
+	LOCATION_LONGITUDE_FIELD
+];
+
+export default class LocationMap extends LightningElement {
+    @api recordId;
+    mapMarkers = [];
+    @wire(getRecord, { recordId: '$recordId', fields: locationFields })
+    loadLocation({ data }) {
+        const Latitude = getFieldValue(data, LOCATION_LATITUDE_FIELD);
+        const Longitude = getFieldValue(data, LOCATION_LONGITUDE_FIELD);
+        this.mapMarkers = [{
+        location: { Latitude, Longitude },
+        }];
+    }
+}
--- force-app/main/default/triggers/LocationTrigger.trigger
+++ force-app/main/default/triggers/LocationTrigger.trigger
@@ -2,5 +2,8 @@
  * @description trigger on location
  */ 
 trigger LocationTrigger on Voting_Location__c (after insert) {
+    /**
+     * @description invoking trigger handler
+     */ 
     new CustomMDTTriggerHandler().run();
 }
