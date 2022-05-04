60f03bc429bac336e33f5229c169fb9c679e2857
punderplus <ilya.obukhov.m@gmail.com>
2022-05-04 20:44:52 +0300

    fixed formating

--- force-app/main/default/lwc/locationMap/locationMap.js
+++ force-app/main/default/lwc/locationMap/locationMap.js
@@ -3,7 +3,6 @@ import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 
 const LOCATION_LATITUDE_FIELD = 'Voting_Location__c.Latitude__c';
 const LOCATION_LONGITUDE_FIELD = 'Voting_Location__c.Longitude__c';
-
 const locationFields = [
 	LOCATION_LATITUDE_FIELD,
 	LOCATION_LONGITUDE_FIELD
@@ -17,7 +16,8 @@ export default class LocationMap extends LightningElement {
         const Latitude = getFieldValue(data, LOCATION_LATITUDE_FIELD);
         const Longitude = getFieldValue(data, LOCATION_LONGITUDE_FIELD);
         this.mapMarkers = [{
-        location: { Latitude, Longitude },
-        }];
+                location: { Latitude, Longitude },
+            }
+        ];
     }
 }
